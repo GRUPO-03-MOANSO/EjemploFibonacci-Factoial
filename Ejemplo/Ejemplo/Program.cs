3f75dfe596493e2faa6d453c15c3e0377a83254e
Dark <jorgeagallardo11@gmail.com>
2022-10-10 14:28:21 +0500

    añadiendo fibonacci

--- Ejemplo/Ejemplo/Program.cs
+++ Ejemplo/Ejemplo/Program.cs
@@ -1,14 +1,46 @@
-﻿using System;
-namespace Ejemplo {
-    internal class Program  {
-        static void Main(string[] args) {
+﻿
+using System;
+namespace Ejemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
             int num = 5;
             Console.WriteLine("El factorial es: ");
             Console.WriteLine(Factorial(num));
             Console.WriteLine();
 
+            Console.WriteLine("El resultado de la suma es: ");
+            Console.WriteLine(NumeroFibonacci(num));
+            Console.ReadLine();
+
         }
-        public static int Factorial(int numero) {
+
+        //FIBONACCI
+        public static int NumeroFibonacci(int posicion)
+        {
+
+            if (posicion > 0)
+            {
+                int num1 = 0, num2 = 1, suma = 0;
+                for (int i = 1; i < posicion; i++)
+                {
+                    suma = num1 + num2;
+                    num1 = num2;
+                    num2 = suma;
+                }
+                return suma;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        //FACTORIAL
+        public static int Factorial(int numero)
+        {
             if (numero > 0)
             {
                 int factorial = 1;
@@ -19,6 +51,7 @@ namespace Ejemplo {
                 return factorial;
             }
             return -1;
+
         }
 
     }
