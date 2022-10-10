27d01020ed97056c1410f3da5fce666199728426
Hector Grados <hectorgradoscorrea@gmail.com>
2022-10-10 12:26:27 -0500

    Agregando primer commit

---
+++ Ejemplo/Ejemplo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Ejemplo {
+    internal class Program  {
+        static void Main(string[] args) {
+            int num = 5;
+            Console.WriteLine("El factorial es: ");
+            Console.WriteLine(Factorial(num));
+            Console.WriteLine();
+
+        }
+        public static int Factorial(int numero) {
+            if (numero > 0)
+            {
+                int factorial = 1;
+                for (int i = 1; i <= numero; i++)
+                {
+                    factorial *= i;
+                }
+                return factorial;
+            }
+            return -1;
+        }
+
+    }
+}
